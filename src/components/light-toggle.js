d132e165802f95553c8ee3ddac1327b489717d0e
Jeffrey <jc2573@cornell.edu>
2019-08-09 17:05:50 -0700

    Begin implementing day/night mode

---
+++ src/components/light-toggle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class LightToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { light: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
